30cc2302a745e7ca0d5cc561abbe42a5cb55912b
naseef434 <naseefp.hin@gmail.com>
2021-08-04 19:23:56 +0530

    hot added

--- gym_management/settings.py
+++ gym_management/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'dfr(4$ng%gau)b8riwadr+w)7-4kyi+ornzf76$^fcg0t%yq$o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
